60fd4af06644c75c3138473c8022354a9db91f23
tushar9919 <ptusharp@yahoo.in>
2016-08-30 12:58:25 +0530

    Added menu type master detail.

--- sample/XamstrapSample/XamstrapSample/MasterDetailSample.xaml.cs
+++ sample/XamstrapSample/XamstrapSample/MasterDetailSample.xaml.cs
@@ -15,17 +15,35 @@ namespace XamstrapSample
             InitializeComponent();
             btnPress.Clicked += BtnPress_Clicked;
             btnPage.Clicked += BtnPage_Clicked;
+            btnWidthChange.Clicked += BtnWidthChange_Clicked;
+            btnToggleMaster.Clicked += BtnToggleMaster_Clicked;
+        }
+
+        private void BtnToggleMaster_Clicked(object sender, EventArgs e)
+        {
+            if (masterDetail.IsMasterVisible)
+                masterDetail.IsMasterVisible = false;
+            else
+                masterDetail.IsMasterVisible = true;
+        }
+
+        private void BtnWidthChange_Clicked(object sender, EventArgs e)
+        {
+            if (masterDetail.ShowDetailHeader)
+                masterDetail.ShowDetailHeader = false;
+            else
+                masterDetail.ShowDetailHeader = true;
         }
 
         private void BtnPage_Clicked(object sender, EventArgs e)
         {
-            masterDetail.IsDetailVisible = true;
+            masterDetail.IsMasterVisible = false;
             masterDetail.DetailContent = new GridPage();
         }
 
         private void BtnPress_Clicked(object sender, EventArgs e)
         {
-            masterDetail.IsDetailVisible = true;
+            masterDetail.IsMasterVisible = false;
             StackLayout sl = new StackLayout();
             sl.Children.Add(new Label() { Text = "New layout added and old removed", HorizontalOptions = LayoutOptions.End, TextColor = Color.Purple });
             masterDetail.DetailContent = sl;
