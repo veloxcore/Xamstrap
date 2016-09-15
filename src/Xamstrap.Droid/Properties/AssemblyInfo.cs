f94416bba365e3a89396dad2aeda4a2bf518ec06
parth7676 <parth7676@gmail.com>
2016-09-15 18:29:31 +0530

    Added nuget.

--- src/Xamstrap.Droid/Properties/AssemblyInfo.cs
+++ src/Xamstrap.Droid/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using Android.App;
 [assembly: AssemblyTitle("Xamstrap.Droid")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Veloxcore")]
 [assembly: AssemblyProduct("Xamstrap.Droid")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
@@ -26,5 +26,5 @@ using Android.App;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
